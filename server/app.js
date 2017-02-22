4336c9a60cb741ac5a6dfb16495446c7d220eeff
Aaron <aagavin@gmail.com>
2017-02-21 23:00:47 -0500

    moved login and register

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ let contact = require('./routes/contact');
 let projects = require('./routes/projects');
 let services = require('./routes/services');
 let docs = require('./routes/docs');
-let login = require('./routes/login');
 
 let app = express();
 
@@ -20,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/../client/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +32,6 @@ app.use('/contact', contact);
 app.use('/projects', projects);
 app.use('/services', services);
 app.use('/docs', docs);
-app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,53 @@
 */
 let express = require('express');
 let router = express.Router();
+let passport = require('passport');
+// define user module
+let User = require('../db/modules/users').User;
 
 /* GET home page. */
-router.get('/', (req, res, next)=>{
-  res.render('index', {  });
+router.get('/', (req, res, next) => {
+  res.render('index', {});
+});
+
+/* GET login page. */
+router.get('/', (req, res, next) => {
+  res.render('login', {});
+});
+
+
+/* GET register page. */
+router.get('/register', (req, res, next) => {
+  res.render('register', {});
+});
+
+/* POST register page. */
+router.post('/register', (req, res, next) => {
+  u = new User({
+    username: req.body.username,
+    email: req.body.email,
+    displayName: req.body.displayName
+
+  });
+
+  User.register(u,
+    req.body.password,
+    (err) => {
+      if (err) { console.log('error adding new user') }
+
+      return passport.authenticate('local')(req, res, () => {
+        res.redirect('/');
+      })
+    });
+});
+
+/* GET login page. */
+router.get('/login', (req, res, next) => {
+  res.render('login', {});
+});
+
+router.post('/login', (req, res, next) => {
+  res.render('login', {});
 });
 
 module.exports = router;
