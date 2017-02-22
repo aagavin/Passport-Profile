af23ff450f6c5e7e347ce470dc046fd83e74e07f
Aaron <aagavin@gmail.com>
2017-02-21 19:16:21 -0500

    add login route

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ let contact = require('./routes/contact');
 let projects = require('./routes/projects');
 let services = require('./routes/services');
 let docs = require('./routes/docs');
+let login = require('./routes/login');
 
 let app = express();
 
@@ -32,6 +33,7 @@ app.use('/contact', contact);
 app.use('/projects', projects);
 app.use('/services', services);
 app.use('/docs', docs);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
