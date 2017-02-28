1c752009f4454a29a880d8b467ac60af5295af35
Aaron <aagavin@gmail.com>
2017-02-28 18:45:51 -0500

    Added contact list schema

---
+++ server/db/modules/contactList.js
@@ -0,0 +1,42 @@
+/*
+  Aaron Fernandes - 300773526 
+  COMP 308 - Assignment 2
+  https://.herokuapp.com/
+
+  This file sets up database information for contact list 
+*/
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let passportLocalMongoose = require('passport-local-mongoose');
+
+
+let ContactSchema = Schema({
+	name: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Contact name is required'
+	},
+	number: {
+		type: Number,
+		default: '9999999999',
+		required: 'Contact number is required'
+	},
+	email: {
+		type: String,
+		default: '',
+		required: 'Contact email is required'
+	}
+	},
+	{
+		collection: 'contacts'
+	}
+)
+
+let options = ({
+	'missingInfo': 'Not all info entered'
+})
+
+ContactSchema.plugin(passportLocalMongoose, options);
+
+exports.ContactList = mongoose.model('ContactSchema', ContactSchema)
